d54c8da62398822c3749339a3b3fd0ea762a9355
Silveira De Melo, Y <y.silveira.de.melo@avanade.com>
2020-08-28 19:12:18 -0300

    continue

--- service-one/src/main/java/br/com/yfsmsystem/serviceone/controller/AppController.java
+++ service-one/src/main/java/br/com/yfsmsystem/serviceone/controller/AppController.java
@@ -10,10 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class AppController {
     private Logger logger = LoggerFactory.getLogger(AppController.class);
+
     @GetMapping
     public ResponseEntity getResource() {
         logger.info("Hello app1");
         RestTemplate restTemplate = new RestTemplate();
         return ResponseEntity.ok().body(restTemplate.getForObject("http://localhost:9002/",String.class));
     }
+    
 }
